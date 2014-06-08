e3fbdb7e94e3d449a75523d9adb47b1a8941fb0a
Stephani Alves <steph@stephanis-pro.gateway.2wire.net>
2014-06-08 11:26:28 -0700

    Initial Commit

---
+++ rottenTomatoes/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  rottenTomatoes
+//
+//  Created by Stephani Alves on 6/8/14.
+//  Copyright (c) 2014 stephanimoroni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ rottenTomatoes/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  rottenTomatoes
+//
+//  Created by Stephani Alves on 6/8/14.
+//  Copyright (c) 2014 stephanimoroni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
